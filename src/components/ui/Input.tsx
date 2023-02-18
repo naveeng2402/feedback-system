d6b2b84b0f7b654c3286af32998ac6973fb03e44
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-02-18 15:33:28 +0530

    Fixed the raised bug.

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,23 +1,37 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface Inputprops {
   name: string;
   placeholder: string;
   type: React.HTMLInputTypeAttribute;
   id: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  Class: string;
 }
 
-const Input: FC<Inputprops> = ({ name, placeholder, type, id }) => {
+const Input: FC<Inputprops> = ({
+  name,
+  placeholder,
+  type,
+  id,
+  value,
+  setValue,
+  Class,
+}) => {
   return (
-    <div>
-      <label htmlFor={id} className="pt-9">
+    <div className="flex flex-col gap-1">
+      <label htmlFor={id} className={Class}>
         {name}
       </label>
+
       <input
         id={id}
         type={type}
         placeholder={placeholder}
-        className="rounded-xl border-2 border-blue-100 mt-1 pt-2 w-80 pl-5 pb-2.5"
+        className="rounded-xl border-2 w-full border-blue-100 mt-1 pt-2  pl-5 pb-2.5 "
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
       ></input>
     </div>
   );

6aa4e742ce11865ee5a43ba6a67a49d006a6972a
Naveen G <naveeng2402@gmail.com>
2023-02-19 19:45:39 +0530

    Added CVA and polymorphism to Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,25 @@ function App() {
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
   return (
-    <div className="mx-auto my-8 w-fit space-y-4">
-      {result.data?.regulationsCollection?.edges.map((elem, idx) => (
-        <p key={elem.node.id}>{elem.node.reg}</p>
-      ))}
-
-      <div className="space-x-4">
-        <Button
-          content="SignIn"
-          clickAction={() => signIn("naveeng2402@gmail.com", "asdf1234")}
-        />
-        <Button content="SignOut" clickAction={signOut} />
+    <div className="mx-auto my-12 w-fit">
+      <div className="space-y-2">
+        {result.data?.regulationsCollection?.edges.map((elem, idx) => (
+          <p key={elem.node.id}>{elem.node.reg}</p>
+        ))}
       </div>
-      <Chevron className="m-4 h-16 w-16 fill-blue-500 transition duration-500 hover:rotate-180" />
+      <div className="m-4 flex gap-6">
+        <Button onClick={() => signIn("naveeng2404@gmail.com", "pass1234")}>
+          <p>Sign In</p>
+          <Chevron className="h-6  w-6 translate-y-0.5 fill-white transition duration-500 hover:rotate-180" />
+        </Button>
+        <Button onClick={signOut} intent="inactive" className="group">
+          <p>Sign Out</p>
+          <Chevron className="h-6 w-6 translate-y-0.5 transition duration-200 group-hover:rotate-180" />
+        </Button>
+        <Button size="circle" className="group p-2">
+          <Chevron className="h-8 w-8 translate-y-[10%] transition group-hover:rotate-180" />
+        </Button>
+      </div>
     </div>
   );
 }

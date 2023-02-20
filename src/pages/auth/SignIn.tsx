8ee6e3f0e15117e8290714e5ac3a7e7023922e75
Naveen G <naveeng2402@gmail.com>
2023-02-20 10:00:36 +0530

    Added SignUp Page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { Button, Input } from "@ui/index";
 import { useState } from "react";
 
@@ -30,13 +30,21 @@ const SignIn = () => {
           setValue={setPass}
         />
       </div>
-      <Button
-        onClick={signIn}
-        size="medium"
-        className="mx-auto w-full px-8 py-4"
-      >
-        Sign In
-      </Button>
+      <div className="space-y-2">
+        <Button
+          onClick={signIn}
+          size="medium"
+          className="mx-auto w-full px-8 py-4"
+        >
+          Sign In
+        </Button>
+        <p className="text-center text-sm">
+          Need a Student account?{" "}
+          <NavLink className="text-blue-700" to="/auth/sign-up">
+            Sign Up
+          </NavLink>
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,5 +1,64 @@
+import { Button, Input } from "@ui/index";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+
+  const signUp = () => {
+    alert(`Email: ${email}\nPass: ${pass}\nConfirm:${confirmPass}`);
+  };
+
+  return (
+    <div className="my-2 mx-auto max-w-sm space-y-8">
+      <div className="flex flex-col gap-4">
+        <h4 className="my-2 text-center text-4xl font-semibold text-blue-700">
+          Student Sign Up
+        </h4>
+        <Input
+          name="Email"
+          id="email"
+          type="email"
+          placeholder=""
+          value={email}
+          setValue={setEmail}
+        />
+        <Input
+          name="Password"
+          id="pass"
+          type="password"
+          placeholder=""
+          value={pass}
+          setValue={setPass}
+        />{" "}
+        <Input
+          name="Confirm Password"
+          id="c_pass"
+          type="password"
+          placeholder=""
+          value={confirmPass}
+          setValue={setConfirmPass}
+        />
+      </div>
+      <div className="space-y-2">
+        <Button
+          onClick={signUp}
+          size="medium"
+          className="mx-auto w-full px-8 py-4"
+        >
+          Sign Up
+        </Button>
+        <p className="text-center text-sm">
+          Already have a account?{" "}
+          <NavLink className="text-blue-700" to="/auth/sign-in">
+            Sign In
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;

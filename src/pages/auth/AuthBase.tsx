aa610ba84c87ec503080c6282acb216bf73fd5aa
Naveen G <naveeng2402@gmail.com>
2023-02-20 08:32:20 +0530

    Added signUp page

--- src/pages/auth/AuthBase.tsx
+++ src/pages/auth/AuthBase.tsx
@@ -11,7 +11,21 @@ const AuthBase = () => {
 
   return (
     <div>
-      Auth Base <Outlet />
+      <div className="flex  min-h-[96vh] flex-col items-center justify-center gap-4 p-2 ">
+        <header className="space-y-4 text-center text-2xl text-blue-800">
+          <h1 className="text-4xl font-semibold text-blue-800">
+            Jaya Engineering College
+          </h1>
+          <h2 className="">Feedback System</h2>
+        </header>
+        <Illustration className="max-h-[40vh] w-3/4" />
+        <main className="w-full px-4">
+          <Outlet />
+        </main>
+      </div>
+      <footer className="m-2 text-sm text-blue-800">
+        2023 JEC All Rights Reserved{" "}
+      </footer>
     </div>
   );
 };

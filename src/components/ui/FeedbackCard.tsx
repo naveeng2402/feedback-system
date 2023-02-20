04ae3ecfd5c49bf7a5d01e111ed72bfd2ec9f5e4
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-02-20 12:25:56 +0530

    Updated FeedbackCard.

--- src/components/ui/FeedbackCard.tsx
+++ src/components/ui/FeedbackCard.tsx
@@ -7,12 +7,12 @@ interface fcProps {
 
 const FeedbackCard: FC<fcProps> = ({ data }) => {
   return (
-    <div className=" grid w-full place-items-center rounded border-2 border-blue-300 ">
-      <div className="py-6 text-center text-xl font-bold text-blue-500 ">
+    <div className=" grid w-full place-items-center rounded-xl border-2 border-blue-300">
+      <div className="  my-6  text-center font-bold text-blue-500 ">
         <h1>Student Feedback Form</h1>
         <h1 className="">({data})</h1>
       </div>
-      <div className="py-6">
+      <div className="py-5">
         <Button>Give Feedback</Button>
       </div>
     </div>

3b554766fae2d6aedbd4e35623e2e46bdb1052ab
Naveen G <naveeng2402@gmail.com>
2023-02-22 12:34:21 +0530

    create student dashboard
    Fixes #36

--- src/pages/stud/DashBoard.tsx
+++ src/pages/stud/DashBoard.tsx
@@ -1,13 +1,13 @@
+import FeedbackCard from "@ui/FeedbackCard";
 import { FC } from "react";
 import { useLocation } from "react-router-dom";
 
 const DashBoard: FC = () => {
-  const location = useLocation();
   return (
-    <div>
-      <p>Dashboard</p>
-      <p>{location.pathname}</p>
-    </div>
+    <main className="space-y-8">
+      <FeedbackCard data="course" action={() => {}} />
+      <FeedbackCard data="lab" action={() => {}} />
+    </main>
   );
 };
 

9aa6824dd2e0cfc0524edece6f724ca6e569bd95
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-02-27 10:28:24 +0530

    Updated the requested changes.

--- src/components/ui/FeedbackAccordion.tsx
+++ src/components/ui/FeedbackAccordion.tsx
@@ -1,38 +1,41 @@
 import { Disclosure } from "@headlessui/react";
 import { FC } from "react";
-import upArrow from "@icons/upArrow.png";
-interface FAProps {
-  RC: React.ReactNode;
+import { ReactComponent as Chevron } from "@icons/Chevron.svg";
+
+interface FeedbackAccordionProps {
+  ratingChip: React.ReactNode;
   Options: OptionsProps[];
   question: string;
 }
 
 interface OptionsProps {
   score: string;
-  image: string;
+  icon: React.ReactNode;
 }
 
-const FeedbackAccordion: FC<FAProps> = ({ RC, Options, question }) => {
+const FeedbackAccordion: FC<FeedbackAccordionProps> = ({
+  ratingChip,
+  Options,
+  question,
+}) => {
   return (
-    <Disclosure as="div" className="border-2 border-blue-600  ">
+    <Disclosure as="div" className="rounded-lg border-2 border-blue-600  px-1 ">
       <Disclosure.Button
         as="div"
         className="flex items-center justify-evenly gap-2"
       >
         <p className="">{question}</p>
 
-        <div className="">{RC}</div>
-        <img src={upArrow} className="h-5 w-5  ui-open:rotate-180" />
+        <div className="">{ratingChip}</div>
+        <Chevron className="h-6  w-6 translate-y-0.5 transition duration-200 ui-open:rotate-180" />
       </Disclosure.Button>
       <Disclosure.Panel className="text-gray-500" as="div">
-        {/* Yes! You can purchase a license that you can share with your entire
-        team. */}
-        <div className=" mx-4 grid grid-cols-5 items-center py-2">
+        <div className=" mx-4 flex items-center gap-4 py-2">
           {Options.map((option) => {
             return (
               <div>
                 <div>
-                  <img src={option.image} alt="" />
+                  {option.icon}
                   <h1 className="mx-2">{option.score}</h1>
                 </div>
               </div>

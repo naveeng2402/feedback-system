53f0f7ce8d95ffd232cee086feac5371d1335bdc
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-03-04 20:10:50 +0530

    Added error404 and error403 pages.

--- src/pages/error/Error403.tsx
+++ src/pages/error/Error403.tsx
@@ -1,17 +1,17 @@
 import { FC } from "react";
-import { ReactComponent as E404 } from "@icons/E404.svg";
+import { ReactComponent as E403 } from "@icons/E403.svg";
 import Button from "@ui/Button";
 
 const Error404: FC = () => {
   return (
     <div className="">
-      <div className="justify-content flex items-center pt-56">
-        <E404 />
+      <div className="grid place-items-center pt-56">
+        <E403 className="" />
       </div>
       <div className=" py-10 text-center">
-        <p className="py-2 text-[#556085]">Oops ! Page not Found</p>
+        <p className="py-2 text-[#556085]">Oops! Access Denied.</p>
         <p className="text-[#A9A9A9]">
-          Seems that the page you are looking for is not exist
+          Looks like you don't have permission to access this resource.
         </p>
       </div>
       <div className="grid place-items-center py-10">

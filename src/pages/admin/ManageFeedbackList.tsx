74009f868f310b2d30db6f50b140c3b551cdb513
Naveen G <naveeng2402@gmail.com>
2023-04-07 19:04:48 +0530

    Added the ManageFeedback Page

--- src/pages/admin/ManageFeedbackList.tsx
+++ src/pages/admin/ManageFeedbackList.tsx
@@ -1,13 +1,22 @@
 import { useManageFeedbackList } from "@/graphql/queries/manageFeedbackList";
 import { FC } from "react";
+import { NavLink } from "react-router-dom";
+import { FeedbackDataItem } from "../stud/Feedback";
 
 const ManageFeedbackList: FC = () => {
   const { data: feedbackList } = useManageFeedbackList();
 
   return (
-    <main>
+    <main className="mx-4 space-y-4">
       {feedbackList?.map((item) => (
-        <pre>{JSON.stringify(item, null, 2)}</pre>
+        <NavLink
+          to={`/admin/feedback/${item.id}`}
+          className="block rounded border-2 border-[#B1C1FD] bg-[#E0EDFF] p-4"
+        >
+          <h2 className="text-lg font-bold text-[#556085]">{`${item.department} - ${item.batch}`}</h2>
+          <FeedbackDataItem prompt="Semester" value={item.sem} />
+          <FeedbackDataItem prompt="Created Date" value={item.created_at} />
+        </NavLink>
       ))}
     </main>
   );

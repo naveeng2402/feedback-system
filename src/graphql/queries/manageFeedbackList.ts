9a74c0b04e8ff12b7f6f2c64c894be588e02c671
Naveen G <naveeng2402@gmail.com>
2023-04-10 04:33:45 +0530

    modified manage feedback query

--- src/graphql/queries/manageFeedbackList.ts
+++ src/graphql/queries/manageFeedbackList.ts
@@ -5,8 +5,18 @@ import { useQuery } from "urql";
 
 const manageFeedbackList = graphql(
   `
-    query manageFeedbackList {
-      feedbackCollection(filter: { is_published: { eq: true } }) {
+    query manageFeedbackList(
+      $is_published: Boolean
+      $batch: Int
+      $dept: BigInt
+    ) {
+      feedbackCollection(
+        filter: {
+          is_published: { eq: $is_published }
+          batch: { eq: $batch }
+          department_id: { eq: $dept }
+        }
+      ) {
         edges {
           node {
             departments {
@@ -23,8 +33,30 @@ const manageFeedbackList = graphql(
   `
 );
 
-export const useManageFeedbackList = () => {
-  const [res, reExecuteQuery] = useQuery({ query: manageFeedbackList });
+interface IManageFeedbackList {
+  batch?: number | null;
+  dept?: number | null;
+}
+
+export const useManageFeedbackList = (
+  is_published: boolean = true,
+  { batch, dept }: IManageFeedbackList = {
+    batch: null,
+    dept: null,
+  }
+) => {
+  let vars: any = { is_published };
+  if (batch) {
+    vars = { ...vars, batch };
+  }
+  if (dept) {
+    vars = { ...vars, dept };
+  }
+
+  const [res, reExecuteQuery] = useQuery({
+    query: manageFeedbackList,
+    variables: vars,
+  });
   const { fetching, data, error } = res;
 
   const dataNorm = useMemo(() => {
